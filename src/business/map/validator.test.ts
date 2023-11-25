cfea26e559f25fe33399da70653cc0ce292043ad
조예진 <orobos654@gmail.com>
2023-11-25 16:49:10 +0900

    refactor: make global value parameter

--- src/business/map/validator.test.ts
+++ src/business/map/validator.test.ts
@@ -3,22 +3,22 @@ import { isValidLevel } from './validator.ts';
 
 describe('isValidLevel', () => {
   test.each([[0], [1], [2], [3], [4], [5]])('%s는 유효한 레벨이다', (receivedLevel) => {
-    const result = isValidLevel(receivedLevel);
+    const result = isValidLevel(receivedLevel, 5);
     expect(result).toBeTruthy();
   });
 
   test('정수가 아니면 유효한 레벨이 아니다', () => {
-    const result = isValidLevel(2.3);
+    const result = isValidLevel(2.3, 5);
     expect(result).toBeFalsy();
   });
 
   test('음수는 유효한 레벨이 아니다', () => {
-    const result = isValidLevel(-1);
+    const result = isValidLevel(-1, 5);
     expect(result).toBeFalsy();
   });
 
   test('최대 레벨보다 크면 유효하지 않다', () => {
-    const result = isValidLevel(6);
+    const result = isValidLevel(6, 5);
     expect(result).toBeFalsy();
   });
 });
--- src/business/map/validator.ts
+++ src/business/map/validator.ts
@@ -1,5 +1,5 @@
 import { MAX_ZOOM_LEVEL } from './constants.ts';
 
-export const isValidLevel = (level: number) => {
-  return Number.isInteger(level) && level >= 0 && level <= MAX_ZOOM_LEVEL;
+export const isValidLevel = (level: number, maxZoomLevel = MAX_ZOOM_LEVEL) => {
+  return Number.isInteger(level) && level >= 0 && level <= maxZoomLevel;
 };
