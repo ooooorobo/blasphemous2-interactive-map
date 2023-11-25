460a4108a07c79b32a65b756a2360ae3246e59be
조예진 <orobos654@gmail.com>
2023-11-25 16:53:58 +0900

    feat: add wheel event listener to zoom

--- src/components/tools/ToolBoxContainer.tsx
+++ src/components/tools/ToolBoxContainer.tsx
@@ -1,14 +1,24 @@
 import { component$, Signal } from '@builder.io/qwik';
 import { isValidLevel } from '../../business/map/validator.ts';
 import { Point } from '../../type/Point.ts';
+import { getZoomScale } from '../../business/map/calculator.ts';
 
 type Props = {
   level: Signal<number>;
-  leftTop: Point;
   screenLeftTop: Point;
+  onClickPoint: (clickedPoint: Point) => void;
 }
 
-export const ToolBoxContainer = component$(({ level, screenLeftTop }: Props) => {
+export const ToolBoxContainer = component$(({ level, screenLeftTop, onClickPoint }: Props) => {
+  const scale = getZoomScale(level.value);
+  window.addEventListener('click', ({ clientX, clientY }) => {
+    const clickedPoint = {
+      x: Math.floor((screenLeftTop.x + clientX) / scale),
+      y: Math.floor((screenLeftTop.y + clientY) / scale),
+    };
+    onClickPoint(clickedPoint);
+  });
+
   return <div style={{ position: 'fixed', top: 0, left: 0, backgroundColor: 'white', color: 'black' }}>
     <strong>tools</strong>
     <div>
--- src/page/app.tsx
+++ src/page/app.tsx
@@ -1,18 +1,27 @@
-import { component$, useSignal, useStore } from '@builder.io/qwik';
+import { $, component$, useSignal, useStore } from '@builder.io/qwik';
 import './app.css';
 import { MapTileLayer } from '../components/layer/MapTileLayer.tsx';
 import { DraggableLayerContainer } from '../components/layer/DraggableLayerContainer.tsx';
 import { Point } from '../type/Point.ts';
 import { ToolBoxContainer } from '../components/tools/ToolBoxContainer.tsx';
 import { MAX_ZOOM_LEVEL } from '../business/map/constants.ts';
+import { throttle } from 'lodash-es';
 import { isValidLevel } from '../business/map/validator.ts';
 
 export const App = component$(() => {
   const level = useSignal(MAX_ZOOM_LEVEL);
 
   const screenLeftTop = useStore<Point>({ x: 0, y: 0 });
+  const markerList = useStore<Point[]>([]);
 
-  const leftTop = useStore<Point>({ x: 0, y: 0 });
+  const debouncedLevelHandler = throttle((action: 'ZOOM_IN' | 'ZOOM_OUT') => {
+    const changedLevel = level.value + 1 * (action === 'ZOOM_IN' ? -1 : 1);
+    if (isValidLevel(changedLevel)) level.value = changedLevel;
+  }, 300, { leading: true });
+
+  window.addEventListener('wheel', ({ deltaY }) => {
+    debouncedLevelHandler(deltaY > 0 ? 'ZOOM_OUT' : 'ZOOM_IN');
+  });
 
   return (
     <div style={{ width: '100%', height: '100%' }}>
@@ -25,9 +34,22 @@ export const App = component$(() => {
       }}>
         <DraggableLayerContainer screenLeftTop={screenLeftTop} level={level.value}>
           <MapTileLayer screenLeftTop={screenLeftTop} level={level.value} />
+          <div>
+            {markerList.map(point => (
+              <div
+                style={{ position: 'absolute', transform: `translate(${point.x}px, ${point.y}px)`, fontSize: '24px' }}>
+                ({point.x}, {point.y})
+              </div>
+            ))}
+          </div>
         </DraggableLayerContainer>
       </div>
-      <ToolBoxContainer level={level} screenLeftTop={screenLeftTop} />
+      <ToolBoxContainer
+        level={level}
+        screenLeftTop={screenLeftTop}
+        onClickPoint={$((point: Point) => {
+          markerList.push(point);
+        })} />
     </div>
   );
 });
